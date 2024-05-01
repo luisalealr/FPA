7cfb2165a6ea20f2e32bf6d5356007415024b09b
luisalealr <luisafernandalr@ufps.edu.co>
2024-04-30 22:25:06 -0500

    index relativamente completo

--- js/login.js
+++ js/login.js
@@ -1,6 +1,7 @@
 const container = document.getElementById('contenedor');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const registerIndex = document.getElementById('registerIndex');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
