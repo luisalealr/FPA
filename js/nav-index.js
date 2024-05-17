0dd424468a4b96341bdc95fcec65dbd26ef109a5
Luisa 3 <106846834+luisalealr@users.noreply.github.com>
2024-05-16 22:01:17 -0500

    ver eventos index

---
+++ js/nav-index.js
@@ -0,0 +1,9 @@
+const nav =document.querySelector('.navegacion')
+
+fetch('../views/navIndex.html')
+.then(res=>res.text())
+.then(data=>{
+    nav.innerHTML=data
+    const parser = new DOMParser()
+    const doc = parser.parseFromString(data, 'text/html')
+})
